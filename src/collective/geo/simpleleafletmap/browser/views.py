8f0e34499c66bfd67e68c9b9a2bff52d134d35b1
David Bain <pigeonflight@gmail.com>
2015-11-02 17:58:11 +0000

    updates simpeleafletmapview

--- src/collective/geo/simpleleafletmap/browser/views.py
+++ src/collective/geo/simpleleafletmap/browser/views.py
@@ -1,71 +1,46 @@
 from collective.geo.geographer.interfaces import IGeoreferenced
 from Products.Five.browser import BrowserView
-from .geomet import wkt
+#from .geomet import wkt
 #from collective.geo.simpleleafletmap.browser.geomet import wkt
 
 class SimpleLeafletMapView(BrowserView):
-    """ this will return coordinates, if the current object is georeferenced """
+    """ this will return a leaflet featureGroup, if the current object is 
+    georeferenced """
     def __init__(self,context,request):
         self.context = context
         self.request = request
         # request.set('disable_border', True)
     
-    def s(self, intup):
-        """ returns a WKT point part - no parens"""
-        return '{} {}'.format(intup[0], intup[1])
+    def flip(self,data):
+        return data[::-1]
+        #(data[1],data[0])
     
-    def gc(self, intup):
-        return ','.join([self.s(m) for m in intup[0]])
-    
-    def flip(self, indata):
-        return (indata[1],indata[0])
-    
-    def cg2ll(self, intype, intup):
+    def collective_geo_to_leaflet(self, intype, intup):
         if intype == 'Point':
-            print intype + '>><<' + str(intup)
             feat = 'L.marker([{}, {}])'.format(intup[1],intup[0])
         
         if intype == 'LineString':
-            print intype + '>><<' + str(intup)
-            #l = eval(intup)
-            t = (str([self.flip(q) for q in intup]).replace('(','[').replace(')',']'))
-            feat = 'L.polyline({})'.format(t)
+            flipped = [self.flip(coord_set) for coord_set in intup]
+            feat = str(flipped).replace('(','[').replace(')',']')
+            feat = 'L.polyline({})'.format(feat)
         
         if intype == 'Polygon':
-            print intype + '>><<' + str(intup)
-            #l = eval(intup)
-            t = (str([self.flip(q) for q in intup[0][:-1]]).replace('(','[').replace(')',']'))
-            feat = 'L.polygon({})'.format(t)
+            flipped = [self.flip(coord_set) for coord_set in intup[0][:-1]]
+            feat = str(flipped).replace('(','[').replace(')',']')
+            feat = 'L.polygon({})'.format(feat)
         
-        
-        print 'feat is :::::::' + feat
         return 'L.featureGroup([{}])'.format(feat)
   
   
-#   def cg2ll(self, intype, intup):
-        
-#         if intype == 'Point':
-#             print intype + '>><<' + str(intup)
-#             return 'L.featureGroup([L.marker([{}, {}])])'.format(intup[1],intup[0])
-      
+
     @property    
     def coordinates(self):
         # using the magic of interfaces
         # we retrieve the georeferenced
         # data from our object
-        obj = IGeoreferenced(self.context, None) # what other methods does obj get?
-        print '>>> ' + str(obj.type) + '::: ' + str(obj.coordinates) + ' <<<'
-        #print '>>> ' + str(self.gc(obj.coordinates)) + ' <<<'
-        #vwkt = obj.type + '((' + str(self.gc(obj.coordinates)) + '))'
-        #need to place the wkt-geojson stuff here
-        #print vwkt.upper()
-        #return wkt.loads(vwkt.upper())  #obj.get
-        return self.cg2ll(obj.type, obj.coordinates)
+        obj = IGeoreferenced(self.context, None) 
+        return self.collective_geo_to_leaflet(obj.type, obj.coordinates)
         
     def js_contextvariables(self):
-        # switches the () for []'s
-        # making it valid json
-        return """<script>var contextvariables = {
-                leafletlayer : %s
-            }
-            </script>""" % str(self.coordinates)
+        js = "<script>var contextvariables = {{leafletlayer : {}}};</script>"
+        return js.format(self.coordinates)
